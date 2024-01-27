9ec34d33b9dfdb127cebadfb7bb775cb994acc17
Devansh <nayyar.devansh@gmail.com>
2024-01-27 21:17:37 +0530

    updates

--- movie-explorer/src/Components/Footer.js
+++ movie-explorer/src/Components/Footer.js
@@ -1,27 +1,20 @@
 import React from 'react';
-import { BsLinkedin,BsGithub,BsTwitter } from "react-icons/bs";
-import { SiHashnode } from "react-icons/si";
+import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <>
-        <div className='flex flex-row justify-around flex-wrap bg-black sticky mt-[400px] bottom-0 w-full'>
-            <p className='text-white text-3xl p-5'>Created with Love By : &copy; "Madhav Sahi"</p>
-            <div className='flex flex-row justify-around '>
-            <a target="_blank" rel="noreferrer"  href= "https://www.linkedin.com/in/madhav-sahi-6a2305161/" 
+      <div className='flex flex-row justify-around flex-wrap bg-black sticky mt-[400px] bottom-0 w-full'>
+        <p className='text-white text-3xl p-5'>Created with Love By : &copy; "Devansh Nayyar"</p>
+        <div className='flex flex-row justify-around '>
+          <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/devanshnayyar/"
             className='text-white text-2xl mr-5 mt-1 p-5 hover:scale-x-125 hover:scale-y-105'>
-            <BsLinkedin/></a>
-            <a target="_blank" rel="noreferrer" href="https://twitter.com/Madhavvv_" 
+            <BsLinkedin /></a>
+          <a target="_blank" rel="noreferrer" href="https://github.com/DevanshNayyar"
             className='text-white text-2xl mr-5 mt-1 p-5 hover:scale-x-125 hover:scale-y-105'>
-            <BsGithub/></a>
-            <a target="_blank" rel="noreferrer" href="https://github.com/MadhavSahi" 
-            className='text-white text-2xl mr-5 mt-1 p-5 hover:scale-x-125 hover:scale-y-105'>
-            <BsTwitter/></a>
-            <a target="_blank" rel="noreferrer" href="https://madhavsahi.hashnode.dev/" 
-            className='text-white text-2xl mr-5 mt-1 p-5 hover:scale-x-125 hover:scale-y-105'>
-            <SiHashnode/></a>
-            </div>
+            <BsGithub /></a>
         </div>
+      </div>
     </>
   )
 }
